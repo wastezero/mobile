e089cd43a06440eb83da26d69541bfdcff365a84
Nursultan Akhmetzhanov <nursultan1998@gmail.com>
2020-10-20 15:58:23 +0600

    feat: add filter view

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   TouchableWithoutFeedback,
   ScrollView,
   View,
+  TextInput,
 } from 'react-native';
-import {Block, Icon, Text} from 'galio-framework';
+import {Block, Button, Icon, Input, Text} from 'galio-framework';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Modal from 'react-native-modal';
+
 import FoodCard from '../../components/FoodCard';
 import {foods} from './mock';
 
 const MenuPage = () => {
+  const [showFilters, setShowFilters] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  const toggleFilters = () => {
+    setShowFilters(!showFilters);
+  };
+
+  const onSearch = (text) => {
+    setSearchValue(text);
+  };
+
   return (
     <View style={styles.container}>
       <SafeAreaView>
-        <TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={toggleFilters}>
           <Block row>
             <Icon family="Ionicons" name="filter-list" size={25} />
             <Text p style={{marginLeft: 10}}>
@@ -22,12 +36,55 @@ const MenuPage = () => {
             </Text>
           </Block>
         </TouchableWithoutFeedback>
+        <Input
+          placeholder="Search"
+          value={searchValue}
+          onChangeText={onSearch}
+          color="#000"
+        />
       </SafeAreaView>
       <ScrollView>
         {foods.map((props, index) => (
           <FoodCard key={index} {...props} />
         ))}
       </ScrollView>
+      <Modal
+        isVisible={showFilters}
+        style={styles.modal}
+        onBackdropPress={toggleFilters}
+        animationIn="slideInDown"
+        animationOut="slideOutUp">
+        <Block style={styles.modalContent}>
+          <SafeAreaView>
+            <View style={styles.inputFieldContainer}>
+              <Text p style={{marginRight: 20}}>
+                Price:
+              </Text>
+              <View style={styles.inputFieldContainer}>
+                <TextInput
+                  placeholder="from"
+                  type="numeric"
+                  style={styles.inputPrice}
+                />
+                <Text>-</Text>
+                <TextInput
+                  placeholder="to"
+                  type="numeric"
+                  style={styles.inputPrice}
+                />
+              </View>
+            </View>
+            <Button
+              color="success"
+              style={{alignSelf: 'center'}}
+              uppercase
+              shadowless
+              onPress={toggleFilters}>
+              Apply Filters
+            </Button>
+          </SafeAreaView>
+        </Block>
+      </Modal>
     </View>
   );
 };
@@ -37,6 +94,29 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingTop: 20,
     flex: 1,
+  },
+  modal: {
+    justifyContent: 'flex-start',
+    margin: 0,
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+  },
+  inputPrice: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#000',
+    width: '30%',
+    marginHorizontal: 10,
+    padding: 5,
+    textAlign: 'center',
+  },
+  inputFieldContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginVertical: 10,
   },
 });
 
